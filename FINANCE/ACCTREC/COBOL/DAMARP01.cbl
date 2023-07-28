f33d7829e46f7a46c6a736596bc0ac3367bd64b5
Thomas McQuitty <you@example.com>
2023-07-28 10:55:04 -0500

    Asd

--- FINANCE/ACCTREC/COBOL/DAMARP01.cbl
+++ FINANCE/ACCTREC/COBOL/DAMARP01.cbl
@@ -1,5 +1,5 @@
 000100 IDENTIFICATION DIVISION.                                                 
-       PROGRAM-ID. MAdSD.                                                    
+       PROGRAM-ID. ads.                                                    
 000300 ENVIRONMENT DIVISION.                                                    
        INPUT-OUTPUT SECTION.                                                    
 000500 FILE-CONTROL.                                                            
